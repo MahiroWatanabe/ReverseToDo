665cae888fd157639f9abfe0edabee4047aa1567
MahiroWatanabe <konchikuwa2@gmail.com>
2023-10-20 22:56:41 +0900

    [add]SearchUserコンポーネント

---
+++ frontend/components/SearchUser/SearchUser.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./SearchUser.module.scss";
+
+const SearchUser = () => {
+  return (
+    <div className={styles["search-container"]}>
+      {/* 検索コード追加 */}
+      Search Component
+    </div>
+  );
+};
+
+export default SearchUser;

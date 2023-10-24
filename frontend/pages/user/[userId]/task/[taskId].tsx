bb41068a8393ca6029f3ccc7b4a61196faa67bc8
MahiroWatanabe <konchikuwa2@gmail.com>
2023-10-24 14:17:09 +0900

    [add]Taskコンポーネントに更新ロジックの追加

--- frontend/pages/user/[userId]/task/[taskId].tsx
+++ frontend/pages/user/[userId]/task/[taskId].tsx
@@ -3,16 +3,23 @@ import React, { useEffect, useState } from "react";
 import {
   getUserTaskData,
   putTaskStatus,
+  putTaskData,
 } from "../../../../utils/userDataRequest";
 import styles from "./task.module.scss";
+import UpdataSide from "../../../../components/UpdateSide/UpdataSide";
 
 const TaskComponent = () => {
   const [useTaskData, setUseTaskData] = useState<ReturnTaskData | null>(null);
   const [isChecked, setIsChecked] = useState(useTaskData?.status == 1);
+  const [openSide, setOpenSide] = useState(false);
 
   const router = useRouter();
   const currentPath: string = router.asPath;
   const pathParts = currentPath.split("/");
+
+  const handleUpdate = () => {
+    setOpenSide((pre) => !pre);
+  };
 
   const handlegetTask = (url: string) => {
     getUserTaskData(url).then((res) => {
@@ -23,13 +30,31 @@ const TaskComponent = () => {
 
   const handleupdateStatus = (url: string, flag: number, id: any) => {
     putTaskStatus(url, flag, id).then((res) => {
-      console.log(url, flag, id);
       setUseTaskData(res);
       return res;
     });
   };
 
-  const handleUpdata = () => {};
+  const handleUpdateTask = (url: string, putTask: ReturnTaskData | null) => {
+    putTaskData(url, putTask).then((res) => {
+      return res;
+    });
+  };
+
+  const handleUpdateTodo = (
+    newTitle: string,
+    newDescription: string,
+    newDeadline: string
+  ) => {
+    if (useTaskData) {
+      setUseTaskData({
+        ...useTaskData,
+        title: newTitle,
+        description: newDescription,
+        deadline: newDeadline,
+      });
+    }
+  };
 
   const handleDelete = () => {};
 
@@ -42,25 +67,37 @@ const TaskComponent = () => {
     handlegetTask(`/task?userid=${pathParts[2]}&taskid=${pathParts[4]}`);
   }, [isChecked]);
 
+  useEffect(() => {
+    handleUpdateTask(`/task`, useTaskData);
+  }, [useTaskData]);
+
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>{useTaskData?.title}</h1>
-      <p className={styles.description}>{useTaskData?.description}</p>
-      <p className={styles.status}>
-        Status: {isChecked ? "Active" : "Inactive"}
-      </p>
-      <p className={styles.deadline}>Deadline: {useTaskData?.deadline}</p>
-      <button onClick={handleUpdata}>Update</button>
-      <button onClick={handleDelete}>Delete</button>
-      <label>
-        <input
-          type="checkbox"
-          className={styles.checkbox}
-          checked={isChecked}
-          onChange={handleCheckChange}
+    <div className={styles["parent-container"]}>
+      <div className={styles["user-task-container"]}>
+        <h1 className={styles.title}>{useTaskData?.title}</h1>
+        <p className={styles.description}>{useTaskData?.description}</p>
+        <p className={styles.status}>
+          Status: {isChecked ? "Active" : "Inactive"}
+        </p>
+        <p className={styles.deadline}>Deadline: {useTaskData?.deadline}</p>
+        <button onClick={handleUpdate}>Update</button>
+        <button onClick={handleDelete}>Delete</button>
+        <label>
+          <input
+            type="checkbox"
+            className={styles.checkbox}
+            checked={isChecked}
+            onChange={handleCheckChange}
+          />
+          Mark as Active
+        </label>
+      </div>
+      {openSide && (
+        <UpdataSide
+          useTaskData={useTaskData}
+          handleUpdateTodo={handleUpdateTodo}
         />
-        Mark as Active
-      </label>
+      )}
     </div>
   );
 };
